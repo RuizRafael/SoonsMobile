ebf2ac566093b778eb7e24ec8c7d53e203450bf7
ManuMenendez99 <manolo.meniba@gmail.com>
2021-06-03 10:42:59 +0200

    Commit Con Modelos De tracking

---
+++ Soons/Soons/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soons.Models
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public String orderNumber { get; set; }
+        public int state { get; set; }
+    }
+}
---
+++ Soons/Soons/Models/ProdsOrder.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soons.Models
+{
+    public class ProdsOrder
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("idOrder")]
+        public int idOrder { get; set; }
+        [JsonProperty("idProd")]
+        public int idProd { get; set; }
+    }
+}
